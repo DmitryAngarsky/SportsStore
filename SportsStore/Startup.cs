cdbeebe29bb22d3102f982cd7c274aa416a6608a
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-10 23:01:30 +0400

    Stage-2
    Adding data to the application, configuring EntityFramework Core, setting up a database connection

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -1,20 +1,31 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SportsStore.Models;
 
 namespace SportsStore
 {
     public class Startup
     {
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<StoreDbContext>(opts =>
+            {
+                opts.UseSqlServer(
+                    Configuration.GetConnectionString("SportsStoreConnection"));
+            });
+            services.AddScoped<IStoreRepository, EFStoreRepository>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -28,6 +39,8 @@ namespace SportsStore
             {
                 endpoints.MapDefaultControllerRoute();
             });
+
+            //SeedData.EnsurePupulated(app);
         }
     }
 }
