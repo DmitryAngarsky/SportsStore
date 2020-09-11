5271f3d1c48d151ffc0526300241bf82f0e7d797
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-11 21:55:37 +0400

    Stage-2.1
    Add unit tests for controller

---
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void Can_Use_Repository()
+        {
+            //Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"}
+            }).AsQueryable<Product>());
+
+            //Act
+            HomeController controller = new HomeController(mock.Object);
+
+            IEnumerable<Product> result = (controller.Index() as ViewResult).ViewData.Model
+                as IEnumerable<Product>;
+
+            //Asert
+            Product[] prodArray = result.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P1", prodArray[0].Name);
+            Assert.Equal("P2", prodArray[1].Name);
+        }
+    }
+}
