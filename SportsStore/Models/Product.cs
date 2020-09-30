d86fba14a64c516b18383496cfbd03b3fcb32fdf
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-30 22:48:12 +0400

    Stage-5.3.2
    Creating the List Component, Creating the Detail Component

--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -15,7 +15,7 @@ namespace SportsStore.Models
         public string Description { get; set; }
         
         [Required]
-        [Range(0.01, Double.MaxValue, 
+        [Range(0.01, double.MaxValue, 
             ErrorMessage = "Please enter a positive price")]
         [Column(TypeName = "decimal(8, 2)")]
         public decimal Price { get; set; }
