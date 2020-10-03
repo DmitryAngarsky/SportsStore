dbaef2c6ac55febe76fd20342bf676b6b3b98fc9
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-10-03 22:13:49 +0400

    Stage-6.1.4
    Creating the Account Controller

---
+++ SportsStore/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models.ViewModels;
+
+namespace SportsStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userMgr,
+            SignInManager<IdentityUser> signInMgr)
+        {
+            _userManager = userMgr;
+            _signInManager = signInMgr;
+        }
+
+        public ViewResult Login(string returnUrl)
+        {
+            return View(new LoginModel
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await _userManager.FindByNameAsync(loginModel.Name);
+                if(user != null)
+                {
+                    await _signInManager.SignOutAsync();
+                    if ((await _signInManager.PasswordSignInAsync(user,
+                        loginModel.Password, false, false)).Succeeded)
+                    {
+                        return Redirect(loginModel?.ReturnUrl ?? "/Admin");
+                    }
+                }
+            }
+
+            ModelState.AddModelError("", "Invalid name or password");
+            return View(loginModel);
+        }
+    }
+}
